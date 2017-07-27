5986119796b9ca48de96e44106a8c0cea679a769
Nikhil Arora <narora200@gmail.com>
2017-07-27 09:17:36 +0530

    input and button added in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  
+  sendData(){
+
+    
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Be-Productive</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <input type="text" ref="action" />
+          <button type="button" onClick={this.sendData.bind(this)}>Send</button>
         </p>
       </div>
     );
