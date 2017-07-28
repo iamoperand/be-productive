a32644d72f275ba4bd8a3ac03dde5cd30ff6f026
Nikhil Arora <narora200@gmail.com>
2017-07-28 08:20:48 +0530

    Sign-up and Log-in boilerplate ready

---
+++ src/components/Authentication/Login/Login.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import './Login.css';
+
+
+
+
+class Login extends Component {
+  
+  constructor(props){
+
+    super(props);
+    
+  }
+
+  
+
+    
+  
+  render() {
+   
+    return null;
+  }
+
+}
+
+export default Login;
---
+++ src/components/Authentication/Signup/Signup.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './Signup.css';
+
+
+class Signup extends Component {
+  
+  constructor(props){
+
+    super(props);
+  }
+    
+  
+  render() {
+   
+    return null;
+  }
+
+  
+}
+
+export default Signup;
