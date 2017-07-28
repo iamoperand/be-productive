2ed859de2cb5bf98a4610ffad989a89f7d1d7055
Nikhil Arora <narora200@gmail.com>
2017-07-28 08:31:50 +0530

    Signup html rendered

--- src/components/Authentication/Signup/Signup.js
+++ src/components/Authentication/Signup/Signup.js
@@ -13,7 +13,25 @@ class Signup extends Component {
   
   render() {
    
-    return null;
+    return (
+    	<div>
+    		<h2>
+    			Sign-up
+    		</h2>
+			<form>
+			  <label>
+			    Email:
+			    <input type="text" name="email" />
+			  </label>
+			  <label>
+			    Password:
+			    <input type="password" name="password" />
+			  </label>
+
+			  <input type="submit" value="Signup" />
+			</form>
+		</div>
+		);
   }
 
   
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import SubmitMotivation from './components/SubmitMotivation/SubmitMotivation';
+import Signup from './components/Authentication/Signup/Signup';
 import registerServiceWorker from './registerServiceWorker';
 
 
-ReactDOM.render(<SubmitMotivation />, document.getElementById('root'));
+ReactDOM.render(<Signup />, document.getElementById('root'));
 registerServiceWorker();
